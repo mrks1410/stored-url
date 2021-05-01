4e3c248adabbef3181ae78a1efbb41eed4ec90ce
mrks1410 <mrks1410@gmail.com>
2021-05-01 23:09:11 +0900

    mod: parse module initializer

--- config/initializers/parse_module_initializer.rb
+++ config/initializers/parse_module_initializer.rb
@@ -1,1 +1,8 @@
+unless FileTest.exist? Rails.root.to_s + '/app/parse_modules/load-module-config.xml'
+    puts '[error]: config file (load-module-config.xml) not found.'
+    puts '[note]: please following command will be run.'
+    puts '    rails module:create_config'
+    exit!
+end
+
 ParseModule::ParseModule.instance
